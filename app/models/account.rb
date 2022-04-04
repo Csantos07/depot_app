a60622bf01a54ac39f7626e232f3c70c1d811112
Carlo Santos <carlo@snapit.solutions>
2022-04-03 22:02:52 -0500

    created devise users, live reload with hotwire in progress

---
+++ app/models/account.rb
@@ -0,0 +1,8 @@
+class Account < ApplicationRecord
+  has_many :user_accounts
+  has_many :users, through: :user_accounts
+
+  after_create_commit { broadcast_prepend_to "accounts" }
+  after_update_commit { broadcast_replace_to "accounts" }
+  after_destroy_commit { broadcast_remove_to "accounts" }
+end
